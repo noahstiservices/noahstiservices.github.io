e32cd6b2cb10770565a3f14c5a321d7fc3540b4f
noahstiservices <noah_stiservices@msn.com>
2025-10-30 10:01:56 -0400

    scoreboard

---
+++ Javascript-Lessons/Scoreboard/index.js
@@ -0,0 +1,35 @@
+let score = document.getElementById("score")
+let result = 0
+function plus1() {
+  result += 1
+  score.textContent = result
+}
+
+function plus2() {
+  result += 2
+  score.textContent = result
+}
+
+function plus3() {
+  result += 3
+  score.textContent = result
+}
+
+let score1 = document.getElementById("score1")
+
+let result1 = 0
+
+function add2() {
+  result1 += 2
+  score1.textContent = result1
+}
+
+function add1() {
+  result1 += 1
+  score1.textContent = result1
+}
+
+function add3() {
+  result1 += 3
+  score1.textContent = result1
+}
---
+++ Javascript-Lessons/variable-challenge/index.js
@@ -0,0 +1,7 @@
+let firstName = "Noah"
+
+let lastName = "Shadle"
+
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
