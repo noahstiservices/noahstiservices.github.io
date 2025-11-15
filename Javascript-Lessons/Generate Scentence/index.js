a2a83bde0886e7d583f7da8b60a0f6224c360d4a
noahstiservices <noah_stiservices@msn.com>
2025-11-14 22:19:08 -0500

    lessons

--- Javascript-Lessons/Generate Scentence/index.js
+++ Javascript-Lessons/Generate Scentence/index.js
@@ -1,11 +1,19 @@
 function generateSentence(desc, arr) {
-  let comibinedString = `The ${arr.length} ${desc} are `
+  combinedString = `The ${arr.length} ${desc} are `
+  //we made lastIndex, so the last word or number in the array doesnt get a coma or anything after it to make it more presentable.
+  const lastIndex = arr.length - 1
   for (let i = 0; i < arr.length; i++) {
-    comibinedString += arr[i] + " "
+    if (i === lastIndex) {
+      // remember that arr[i] is saying we only want the array and we already are calling for the length in the for loop above, so we dont need to do it again.
+      combinedString += arr[i]
+    } else {
+      combinedString += arr[i] + ", "
+    }
   }
-  return comibinedString
+  return combinedString
 }
 
-const sentence = generateSentence("rarest mushrooms", ["fungus ", "white"])
+const bigSentence = generateSentence("Best Food Places", ["Pizza Joes", "Applebees", "Wendys"])
 
-console.log(sentence)
+console.log(bigSentence)
+
